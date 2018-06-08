3d10f69c80320bda7fb57102e6501fe3edaa8394
Zach Freed <zach.freed@gmail.com>
2018-06-07 20:25:42 -0400

    almost done with postcss plugin installs

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,20 @@
 //var $ = require('gulp-load-plugins')();
 
 var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var sourcemaps = require('gulp-sourcemaps');
+var runSequence = require('run-sequence');
+
+const advancedVars = require('postcss-advanced-variables');
+const apply = require('postcss-apply');
+const calc = require('postcss-calc');
+const cssimport = require('postcss-import');
+const cssnano = require('cssnano');
+const customProps = require('postcss-custom-properties');
+const nesting = require('postcss-nesting');
+const nestedProps = require('postcss-nested-props');
+const presetEnv = require('postcss-preset-env');
+
 
 gulp.task('scripts', function () {
 });
@@ -13,10 +27,36 @@ gulp.task('html', function () {
 });
 
 gulp.task('styles', function (cb) {
-    return gulp.src(
-        './*.sass',
-        './*.scss'
-    )
+
+  return gulp.src('src/**/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(postcss([
+      cssimport(),
+      customProps({
+        preserve: false,
+        warnings: true
+      }),
+      apply(),
+      calc(),
+      nesting(),
+      nestedProps(),
+      cssnano({
+        preset: 'default'
+      }),
+      presetEnv({
+        browsers: [
+          'last 2 versions',
+          'ie >= 8',
+          'dead'
+        ],
+        stage: 3,
+        autoprefixer: ({
+          grid: true
+        })
+      })
+    ]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('public/'));
 });
 
 gulp.task('images', function (cb) {
@@ -28,11 +68,15 @@ gulp.task('fonts', function () {
 gulp.task('test', function () {
 });
 
+gulp.task('clean', function () {
+});
+
 gulp.task('watch', function () {
 });
 
 // just test to make sure the file is alive
 gulp.task('default', function () {
+  runSequence('styles')
 });
 
 // spin a server
