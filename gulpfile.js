9c82d786bb82c971a370c048d2c1b3db98b9ba1a
Zach Freed <zach.freed@gmail.com>
2018-07-14 21:25:07 -0400

    more stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,30 @@
-// gonna try to do this without the loader this time
-
-var gulp = require('gulp');
-var postcss = require('gulp-postcss');
-var $ = require('gulp-load-plugins')();
+// stimpleport gulpfile
+//
+// by @ponchofreedo
+//
+//
+const gulp = require('gulp');
+const gulpLoadPlugins = require('gulp-load-plugins');
+const browserSync = require('browser-sync').create();
 const runSequence = require('run-sequence');
+const wiredep = require('wiredep').stream;
 
+const postcss = require('gulp-postcss');
 const advancedVars = require('postcss-advanced-variables');
 const apply = require('postcss-apply');
 const calc = require('postcss-calc');
-const cssimport = require('postcss-import');
+const partials = require('postcss-import');
 const cssnano = require('cssnano');
-const customProps = require('postcss-custom-properties');
-const nesting = require('postcss-nesting');
 const nestedProps = require('postcss-nested-props');
 const presetEnv = require('postcss-preset-env');
 
+const $ = gulpLoadPlugins();
+const reload = browserSync.reload;
+
+
+
+// to switch between src and public
+// let dev = true;
 
 gulp.task('scripts', function () {
 });
@@ -22,22 +32,17 @@ gulp.task('scripts', function () {
 gulp.task('gzip', function() {
 });
 
-gulp.task('styles', function (cb) {
+gulp.task('postcss', function (cb) {
   return gulp.src('src/**/*.css')
-    .pipe($.sourcemaps.init())
+    .pipe($.if(dev, $.sourcemaps.init()))
     .pipe(postcss([
-      cssimport(),
-      customProps({
-        preserve: false,
-        warnings: true
+      partials({
+        'skipDuplicates': true
       }),
-      apply(),
-      calc(),
-      nesting(),
+      apply({
+        preserve: false
+      }),
       nestedProps(),
-      cssnano({
-        preset: 'default'
-      }),
       presetEnv({
         browsers: [
           'last 2 versions',
@@ -45,14 +50,30 @@ gulp.task('styles', function (cb) {
           'dead'
         ],
         stage: 3,
+        features: {
+          'custom-properties': {
+            preserve: false,
+            warnings: true
+          },
+          'nesting-rules': true
+        },
         autoprefixer: ({
           grid: true
         })
+      }),
+      calc({
+        warnWhenCannotResolve: true
+      }),
+      cssnano({
+        autoprefixer: false,
+        preset: 'default',
+        safe: true
       })
     ]))
-    .pipe($.sourcemaps.write('.'))
-    .pipe(gulp.dest('public/'))
-    .pipe($.size({ title : 'css' }));
+    .pipe($.if(dev, $.sourcemaps.write('.')))
+    .pipe(gulp.dest('.tmp/styles'))
+    .pipe($.size({ title : 'css' }))
+    .pipe(reload({stream: true}));
 });
 
 gulp.task('html', function () {
@@ -78,11 +99,18 @@ gulp.task('watch', function () {
 
 // just test to make sure the file is alive
 gulp.task('default', function () {
-  runSequence('styles')
+  runSequence('postcss')
 });
 
 // spin a server
 gulp.task('serve', function () {
+  browserSync.init({
+    notify: false,
+    port: 9000,
+    server: {
+      baseDir: ['public']
+    }
+  });
 });
 
 // put a bow on it
