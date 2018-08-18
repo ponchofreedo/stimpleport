36bd7ce8cc434ce207df742019581bf14974261d
Zach Freed <zach.freed@gmail.com>
2018-08-18 19:13:43 -0400

    do over

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,10 @@ const gulpLoadPlugins = require('gulp-load-plugins');
 const browserSync = require('browser-sync').create();
 const runSequence = require('run-sequence');
 const wiredep = require('wiredep').stream;
-const cssnano = require('cssnano');
+const del = require('del');
+
+const postcss = require('gulp-postcss');
+const presetEnv = require('postcss-preset-env');
 
 const $ = gulpLoadPlugins();
 const reload = browserSync.reload;
@@ -27,13 +30,41 @@ gulp.task('sass', () => {
       precision: 10,
       includePaths: ['.']
     }).on('error', $.sass.logError))
+    .pipe($.if(dev, $.sourcemaps.write()))
+    .pipe($.cssnano({
+      preset: 'default',
+      safe: true
+    }))
+    .pipe(postcss([
+      presetEnv({
+        browsers: [
+          'last 2 versions',
+          'ie >= 8',
+          'dead'
+        ],
+        autoprefixer: ({
+          grid: true,
+          flexbox: true
+        })
+      })
+    ]))
+    .pipe(gulp.dest('public/css'))
     .pipe($.size({ title : 'sass' }))
-    .pipe(gulp.dest('.tmp/css'))
     .pipe(reload({stream: true}));
 });
 
 gulp.task('html', () => {
   return gulp.src('src/*.html')
+    .pipe($.if('*.html', $.htmlmin({
+      collapseWhitespace: true,
+      minifyCSS: true,
+      minifyJS: {compress: {drop_console: true}},
+      processConditionalComments: true,
+      removeComments: true,
+      removeEmptyAttributes: true,
+      removeScriptTypeAttributes: true,
+      removeStyleLinkTypeAttributes: true
+    })))
     .pipe(gulp.dest('public'))
     .pipe($.size({ title : 'html' }))
 });
@@ -41,49 +72,37 @@ gulp.task('html', () => {
 gulp.task('images', function (cb) {
   return gulp.src('src/img/**/*.{png,jpg,svg,jpeg,gif,ico}')
     .pipe($.size({ title : 'images' }))
-    .pipe(gulp.dest('.tmp/img'));
+    .pipe(gulp.dest('public/img'));
 });
 
 gulp.task('test', () => {
 });
 
-gulp.task('clean', () => {
-});
-
-gulp.task('watch', () => {
-});
-
 gulp.task('scripts', () => {
   return gulp.src('src/scripts/**/*.js')
     .pipe($.if(dev, $.sourcemaps.init()))
-    .pipe(gulp.dest('.tmp/scripts'))
+    .pipe(gulp.dest('public/scripts'))
     .pipe(reload({stream: true}));
-});
-
-gulp.task('extras', () => {
-  return gulp.src([
-    'src/*',
-    '!src/*.html'
-  ], {
-    dot: true
-  }).pipe(gulp.dest('public'));
 });
 
 // spin a server
 gulp.task('serve', () => {
-  browserSync.init({
-    notify: false,
-    port: 9000,
-    server: ['.tmp', 'src']
+  runSequence(['sass', 'scripts', 'images'], () => {
+    browserSync.init({
+      notify: false,
+      port: 9000,
+      server: ['src', 'public']
+    });
+
+    gulp.watch([
+      'src/*.html',
+      'src/img/**/*'
+    ]).on('change', reload);
+
+    gulp.watch('src/css/**/*.scss', ['sass']);
+    gulp.watch('src/scripts/**/*.js', ['scripts']);
+    gulp.watch('src/css/img/**/*.{png,jpg,svg,jpeg,gif,ico}', ['images']);
   });
-
-  gulp.watch([
-    'src/*.html',
-  ]).on('change', reload);
-
-  gulp.watch('src/css/**/*.scss', ['sass']);
-  gulp.watch('src/scripts/**/*.js', ['scripts']);
-  gulp.watch('src/css/img/**/*.{png,jpg,svg,jpeg,gif,ico}', ['images']);
 });
 
 // just test to make sure the file is alive
@@ -93,7 +112,9 @@ gulp.task('default', () => {
 });
 
 // put a bow on it
-gulp.task('build', () => {
+gulp.task('build', ['html', 'sass', 'images', 'scripts'], () => {
+  return gulp.src('src/**/*')
+    .pipe($.size({ title: 'build' }));
 });
 
 // ['build', 'watch']
