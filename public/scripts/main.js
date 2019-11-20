20153c8535e5bc84688a23e71105585cb5eed948
Zach Freed <zach.freed@gmail.com>
2019-11-20 00:55:42 -0500

    getting up to speed

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,16 @@
 console.log("im working");
+
+// mode switcher
+const viewId = document.querySelector('#project');
+const toggleMode = document.querySelector('.theme__switch');
+const currentMode = localStorage.getItem('theme');
+
+document.body.setAttribute('data-theme', currentMode);
+
+if (viewId) {
+  document.body.setAttribute('data-theme', 'light');
+} else {
+  document.body.setAttribute('data-theme', 'dark');
+}
+
+localStorage.setItem('theme', 'dark');
